3f1c65d506b1bd846c9ccf00ee464a0420f0881c
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-11-10 07:02:22 +0530

    JavaScript Basics, If Else, If Else Ladder

---
+++ Day_24/script.js
@@ -0,0 +1,42 @@
+console.log("Abhishek Nayak");
+
+console.log("Avantika Lizzie Yashica");
+
+console.log(9 * 5 * 5 - 6 - 1 + 12348);
+
+// Variable is a container in the memory which stores some data
+// Types of variable declarations :  let, const
+
+const name = "Abhishek Nayak";
+let age = 25;
+
+age = 29;
+
+console.log(name);
+console.log(age + 2);
+
+// CONDITIONALS STATEMENTS
+// If and Else
+
+const ManasAge = 18;
+
+if (ManasAge > 18) {
+  console.log("You can drive car");
+} else {
+  console.log("You are not allowed to drive");
+}
+
+// If and Else Ladder
+let favorite = "Ice cream";
+
+if (favorite === "Burger") {
+  console.log("Fine");
+} else if (favorite === "Pizza") {
+  console.log("Great");
+} else if (favorite === "Ice cream") {
+  console.log("Nice");
+} else {
+  console.log("Nothing Favorite");
+}
+
+
