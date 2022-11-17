35895a99e8af9cafff8c81f8b494c49e21c25a5e
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-11-17 07:12:08 +0530

    JavaScript Event Listeners

---
+++ Day_26/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector(".btn");
+const box = document.querySelector(".box");
+
+button.addEventListener("click", function () {
+  box.classList.toggle("active2");
+});
+
+// HOMEWORK
+// 1. Create a image gallery and change all the images on a single click of button.
---
+++ Day_26/work.js
@@ -0,0 +1,31 @@
+let a = 5;
+let b = 6;
+// console.log(a + b);
+
+a = 12;
+b = 1;
+// console.log(a + b);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(57, 3);
+add(82, 16);
+add(5119, 19191919234);
+add(534567619, 19191919234);
+
+const addMe = (a, b) => {
+  console.log(a + b);
+};
+
+// CALCULATIONS IN JAVASCRIPT
+console.log("12345678" + 5);
+console.log("5" * 5);
+console.log("5" - 5);
+console.log("5" / 5);
+
+console.log("book" + 5);
+console.log("book" * 5); // NAN (NOT A NUMBER)
+console.log("book" - 5); // NAN (NOT A NUMBER)
+console.log("book" / 5); // NAN (NOT A NUMBER)
